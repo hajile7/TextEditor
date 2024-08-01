5a5ae0e318b8aaedffa112804ca8c0bf519a2be5
Eli <elireid.28@gmail.com>
2024-08-01 15:37:31 -0700

    very basic doc started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DocComponent } from './Components/doc/doc.component';
+import { ToolbarComponent } from './Components/toolbar/toolbar.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DocComponent, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DocComponent } from './Components/doc/doc.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "Doc", component: DocComponent},
+];
